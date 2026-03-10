c4b59e732b183bdd0edbaf9f817634363b15cea7
Jeyaram A <ramram13052001@gmail.com>
2026-03-10 22:58:59 +0530

    Your commit message here

--- TrainingInstituteLMS.Web/src/components/student/PublicEnrollmentForm.tsx
+++ TrainingInstituteLMS.Web/src/components/student/PublicEnrollmentForm.tsx
@@ -305,7 +305,7 @@ export function PublicEnrollmentForm({ onComplete, onCancel }: PublicEnrollmentF
       postalAddress: applicant.postAddress || undefined,
       postalSuburb: applicant.postSuburb || undefined,
       postalState: applicant.postState || undefined,
-      postalPostcode: applicant.postPostcode || undefined,
+      postalCodeOptional: applicant.postPostcode || undefined,
       emergencyContactName: applicant.emergencyName || '',
       emergencyContactRelationship: applicant.emergencyRelationship || '',
       emergencyContactNumber: applicant.emergencyContactNumber || '',
